b77732527ff3852d25a1027453e31aea8d9d7a5f
codifyinstitute <166363972+codifyinstitute@users.noreply.github.com>
2024-09-27 23:37:29 +0530

    Update Sell1.js

--- src/components/Sell1.js
+++ src/components/Sell1.js
@@ -696,8 +696,9 @@ const Sell1 = () => {
           const feesData = await feesResponse.json();
           setTransactionFee(feesData.TransactionFee);
           setNetworkFee(feesData.NetworkFee);
+            setUsdt(feesData.MinAmount);
           setMinAmount(feesData.MinAmount);
-           setUsdt(data.MinAmount)
+         
         }
       } catch (error) {
         console.error('Error fetching data:', error);
